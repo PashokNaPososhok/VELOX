747d4d9b8431c80109fab625f5c3347a9c4ae7ea
PashokNaPososhok <153077006+PashokNaPososhok@users.noreply.github.com>
2026-03-11 08:50:26 +0400

    VELOX

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -206,35 +206,14 @@
 
     <div class="home-hero">
         <div id="carouselExampleDark" class="carousel carousel-dark slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
 
             <div class="carousel-inner">
                 @foreach($array as $a)
-                    @if($loop->index==0)
                         <div class="carousel-item active" data-bs-interval="10000">
                             <img src="../public/{{$a->image}}" class="d-block w-100" alt="...">
                         </div>
-                    @else
-                        <div class="carousel-item" data-bs-interval="2000">
-                            <img src="../public/{{$a->image}}" class="d-block w-100" alt="...">
-                        </div>
-                    @endif
                 @endforeach
             </div>
-
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
 
         <div class="hero-content">
